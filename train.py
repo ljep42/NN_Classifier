03c9674fc9079f10e73727779e7e3d559d1bd524
lee <19956144+ljep42@users.noreply.github.com>
2021-08-30 09:51:22 -0400

    added main and more cleanup

--- train.py
+++ train.py
@@ -9,21 +9,21 @@ import math
 
 def display_graph(x, y, num):
     '''
-    Used for debugging/learning.  Displays resized image of MNIST number specified by the parameter num
-    :param x: features array/matrix
-    :param y: target array/matrix
+    Used for debugging/learning.  Displays 100 resized images of MNIST target/true number specified by the parameter num
+    :param x: features array/matrix, 784 pixel (columns) by 60000 examples (rows)
+    :param y: target array/matrix, 1 number (column) by 60000 examples (rows)
     :param num: number of interest you want to see displayed
     :return: outputs matplotlib window object
     '''
 
     number = num
-    b = np.argwhere(y[0:1000] == number)
-    rows = 20
+    indices = np.argwhere(y.to_numpy() == number)[:100]
+    rows = 10
     idx = 1
     fig = plt.figure()
-    for val in np.nditer(b):
-        fig.add_subplot(rows, math.ceil(b.size / rows), idx)
-        plt.imshow(np.resize(x[val], (28, 28)))
+    for val in np.nditer(indices):
+        fig.add_subplot(rows, math.ceil(indices.size / rows), idx)
+        plt.imshow(np.resize(x[val.item():val.item()+1], (28, 28)))
         idx += 1
 
     plt.show()
@@ -34,29 +34,30 @@ def train_neural():
     :return: pickle file save file in project folder path
     '''
 
-    # load data
+    # load data from openML.org
     mnist_data = fetch_openml('mnist_784', version=1)
 
     # assign features and labels
     X, y = mnist_data['data'], mnist_data['target']
     #print('Shape of X:', X.shape, '\n', 'Shape of y:', y.shape)
 
-    # scale set
+    # scale set to range [0,1] like MinMaxScaler
     X = X / 255.0
 
-    # split into test and train into 75/25
+    # split into test and train
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=10000)
 
-    #display_graph(X_train, y_train, 7)
+    # use for debugging
+    #display_graph(X_train, y_train, '7')
 
-    # instantiate estimator (Multi layer perceptron)
+    # instantiate estimator (Multi Layer Perceptron Classifier)
     model = MLPClassifier(hidden_layer_sizes=(100, 50, 20), max_iter=100, verbose=False, activation='relu',
                           solver='adam', early_stopping=True, learning_rate='adaptive', random_state=1)
     # fit with data
     print('Training...')
     model.fit(X_train, y_train)
 
-    # print metrics
+    # display metrics
     print('train score: ', model.score(X_train, y_train))
     pred = model.predict(X_test)
     print('test score: ', model.score(X_test, pred))
@@ -66,6 +67,11 @@ def train_neural():
     filename = 'mnist_ML_model.sav'
     pickle.dump(model, open(filename, 'wb'))
 
+def main():
+    train_neural()
+    print('Complete!')
 
-train_neural()
-print('Complete!')
+if __name__ == "__main__":
+    main()
+
+
